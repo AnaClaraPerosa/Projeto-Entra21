0f4599a55ccc653b4949ea3a867de1efb81a3dc1
Jaqueline Peres <104083695+jaquealt@users.noreply.github.com>
2022-09-08 11:27:03 -0300

    criacao dos models

--- DjangoMKT/fornecedores/models.py
+++ DjangoMKT/fornecedores/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from cidades.models import Cidades
 
-# Create your models here.
+#Create your models here.
+
+class Fornecedores(models.Model):
+    fornecedor_razao_social = models.CharField(max_length=100, verbose_name='Razão social')
+    fornecedor_cpf_cnpj = models.CharField(max_length=20, verbose_name='CPFJ/CNPJ')
+    fornecedor_email = models.CharField(max_length=100, verbose_name='Email')
+    fornecedor_telefone = models.CharField(max_length=30, verbose_name='Telefone')
+    fornecedor_logradouro = models.CharField(max_length=100, verbose_name='Logradouro')
+    fornecedor_bairro = models.CharField(max_length=100, verbose_name='Bairro')
+    fornecedor_numero = models.IntegerField(default=0, verbose_name='Número')
+    fornecedor_cep = models.IntegerField(default=0, verbose_name='CEP')
+    fornecedor_complemento = models.CharField(max_length=100, verbose_name='Complemento')
+    fornecedor_obs = models.CharField(max_length=100, verbose_name='Observações',default='')
+    fornecedor_cidade_id = models.ForeignKey(Cidades,verbose_name='Cidade_ID', on_delete=models.PROTECT)
+    fornecedor_ativo = models.CharField(max_length=1)
+
+    def __str__(self):
+        return '%s %s' % (self.fornecedor_razao_social, self.fornecedor_cpf_cnpj)
+
+    class Meta:
+        ordering = (['fornecedor_razao_social', 'fornecedor_cpf_cnpj'])
+        verbose_name = 'Fornecedor'
+        verbose_name_plural = 'Fornecedores'
+
--- DjangoMKT/produtos/models.py
+++ DjangoMKT/produtos/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+#Create your models here.
+
+
+class Segmentos(models.Model):
+    segmento_nome = models.CharField(max_length=100,verbose_name='Segmento')
+    segmento_ativo = models.CharField(max_length=1)
+
+    def __str__(self):
+        return '%s' % (self.segmento_nome)
+
+    class Meta:
+        ordering = (['segmento_nome'])
+        verbose_name = 'Segmento'
+        verbose_name_plural = 'Segmentos'
+
+
+class Produtos(models.Model):
+    produto_nome = models.CharField(max_length=100,verbose_name='Produto')
+    produto_descricao = models.CharField(max_length=100,verbose_name='Descrição')
+    produto_valor_unit = models.IntegerField(default=0,verbose_name='Valor Unitário')
+    produto_qtde = models.IntegerField(default=0,verbose_name='Quantidade')
+    produto_segmento_id = models.ForeignKey(Segmentos,verbose_name='Segmento_ID', on_delete=models.PROTECT)
+    produto_ativo = models.CharField(max_length=1)
+
+    def __str__(self):
+        return '%s %s' % (self.produto_nome, self.produto_descricao)
+
+    class Meta:
+        ordering = (['produto_nome','produto_descricao'])
+        verbose_name = 'Produto'
+        verbose_name_plural = 'Produtos'
