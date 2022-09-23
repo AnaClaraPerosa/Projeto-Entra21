15b2d946ba399619747b65584c48a53c8ef04a8b
Renan Oliveira <osilveira.renan@gmail.com>
2022-09-23 10:49:06 -0300

    teste true

--- proj/settings.py
+++ proj/settings.py
@@ -29,7 +29,7 @@ except ImportError:
     pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','ipicles.herokuapp.com']
 
