e55f73a0988e8eaf7aeccaf98f2fdecd1a74a57a
Renan Oliveira <osilveira.renan@gmail.com>
2022-10-03 19:34:25 -0300

    delete home

--- proj/settings.py
+++ proj/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home',
     'fornecedores',
     'produtos',
     'carrinho',
