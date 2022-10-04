7a73c4e562f7721455bac05af00614b3cc44a52e
Lucas Salvaro Dimon <lsdimon66@gmail.com>
2022-10-04 10:42:47 -0300

    models_produtos

--- produtos/models.py
+++ produtos/models.py
@@ -1,33 +1,65 @@
+from io import BytesIO
 from django.db import models
+from PIL import Image
+from django.core.files import File
+from django.db import models
+from fornecedores.models import Fornecedores
 
 #Create your models here.
 
 
-class Segmentos(models.Model):
-    segmento_nome = models.CharField(max_length=100,verbose_name='Segmento')
-    segmento_ativo = models.CharField(max_length=1)
+class Categoria(models.Model):
+    titulo = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    ordem = models.IntegerField(default=0)
+
+    
+    class Meta:
+        ordering = ['ordem']
 
     def __str__(self):
-        return '%s' % (self.segmento_nome)
+        return self.titulo
+
+
+class Produto(models.Model):
+    categoria = models.ForeignKey(Categoria, related_name='produtos', on_delete=models.CASCADE)
+    fornecedor = models.ForeignKey(Fornecedores, related_name='produtos', on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    descricao = models.TextField(blank=True, null=True)
+    preco = models.DecimalField(max_digits=6, decimal_places=2)
+    data_add = models.DateTimeField(auto_now_add=True)
+    imagem = models.ImageField(upload_to ='uploads/', blank = True, null = True)
+    thumbnail = models.ImageField(upload_to ='uploads/', blank = True, null = True)
+
 
     class Meta:
-        ordering = (['segmento_nome'])
-        verbose_name = 'Segmento'
-        verbose_name_plural = 'Segmentos'
-
-
-class Produtos(models.Model):
-    produto_nome = models.CharField(max_length=100,verbose_name='Produto')
-    produto_descricao = models.CharField(max_length=100,verbose_name='Descrição')
-    produto_valor_unit = models.IntegerField(default=0,verbose_name='Valor Unitário')
-    produto_qtde = models.IntegerField(default=0,verbose_name='Quantidade')
-    produto_segmento_id = models.ForeignKey(Segmentos,verbose_name='Segmento_ID', on_delete=models.PROTECT)
-    produto_ativo = models.CharField(max_length=1)
+        ordering = ['-data_add']
 
     def __str__(self):
-        return '%s %s' % (self.produto_nome, self.produto_descricao)
+        return self.titulo
 
-    class Meta:
-        ordering = (['produto_nome','produto_descricao'])
-        verbose_name = 'Produto'
-        verbose_name_plural = 'Produtos'
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return self.thumbnail.url
+        else:
+            if self.imagem:
+                self.thumbnail = self.make_thumbnail(self.imagem)
+                self.save()
+
+                return self.thumbnail.url
+            else:
+                return 'https://via.placeholder.com/240x180.jpg'
+    
+    def make_thumbnail(self, image, size=(300, 200)):
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+
+        thumbnail = File(thumb_io, name=image.name)
+
+        return thumbnail
+
