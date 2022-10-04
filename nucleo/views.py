2ee90e0bed248dfdd369a63730e1ca349a1ac75a
Jaqueline Peres <104083695+jaquealt@users.noreply.github.com>
2022-10-04 10:23:26 -0300

    prerarando views da app nucleo

--- nucleo/views.py
+++ nucleo/views.py
@@ -1,3 +1,39 @@
+from multiprocessing import context
+from queue import Empty
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-# Create your views here.
+from produtos.models import Produto
+
+
+def index(request):
+    return render(request,'index.html')
+
+def contato(request):
+    return render(request,'contato.html')
+
+def loja(request):
+    parametro_page = request.GET.get('page', '1')
+    parametro_limit = request.GET.get('limit', '8')
+
+
+    if not (parametro_limit.isdigit() and (int(parametro_limit) > 0)):
+        parametro_limit = '8'
+
+    # novos_produtos = Produto.objects.all()[0:8]
+    novos_produtos = Produto.objects.all()
+
+    produtos_paginator = Paginator(novos_produtos, parametro_limit)
+    
+    try:
+        page = produtos_paginator.page(parametro_page)
+    except (EmptyPage, PageNotAnInteger):
+        page = produtos_paginator.page(1)
+
+
+    context = {
+        'novos_produtos' : page
+    }
+
+    return render(request,'loja.html', context)
+    
