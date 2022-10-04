7afa3a1f8fe5560c2833aa7e566dcbb0d5716c83
Ana Clara <anaclaraperosa@gmail.com>
2022-10-04 17:00:01 -0300

    ajustes no models clientes

--- clientes/models.py
+++ clientes/models.py
@@ -1,7 +1,12 @@
 from django.db import models
-from cidades.models import Cidades
+from endereco.models import Cidades
+from django.contrib.auth.models import User
 
 #Create your models here.
+
+def user_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'usuario_{0}/{1}'.format(instance.user.id, filename)
 
 class Clientes(models.Model):
     cliente_nome = models.CharField(max_length=100,verbose_name='Nome')
@@ -14,11 +19,12 @@ class Clientes(models.Model):
     cliente_cep = models.IntegerField(default=0, verbose_name='CEP')
     cliente_complemento = models.CharField(max_length=100, verbose_name='Complemento')
     cliente_obs = models.CharField(max_length=100, verbose_name='Observações')
-    cliente_cidade_id = models.ForeignKey(Cidades,verbose_name='Cidade_ID', on_delete=models.PROTECT)
+    cliente_cidade_id = models.ForeignKey(Cidades,verbose_name='Cidade', on_delete=models.PROTECT)
     cliente_ativo = models.CharField(max_length=1)
+    usuario = models.ForeignKey(User, default=0, on_delete=models.PROTECT, verbose_name='cliente')
 
     def __str__(self):
-        return '%s %s' % (self.cliente_nome, self.cliente_telefone)
+        return '%s %s' % (self.cliente_nome, self.cliente_telefone, self.cliente_cpf)
 
     class Meta:
         ordering = (['cliente_nome', 'cliente_telefone'])
