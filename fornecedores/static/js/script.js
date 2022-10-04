bc844feae96d1d41c56c3c34281dc844af9f66aa
Renan Oliveira <osilveira.renan@gmail.com>
2022-10-04 10:15:48 -0300

    static app fornecedores

---
+++ fornecedores/static/js/script.js
@@ -0,0 +1,11 @@
+function loja(){
+    alert("Incluir aqui no arquivo script. js o link para a Loja!")
+}
+
+function busca(){
+    alert("Incluir aqui no arquivo script. js o link para a Busca!")
+}
+
+function news(){
+    alert("Incluir aqui no arquivo script. js o link para o News!")
+}
