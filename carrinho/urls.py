cc81168661033027efb06ed03c158266d4f1841b
Renan Oliveira <osilveira.renan@gmail.com>
2022-10-06 21:11:41 -0300

    pequena alt nas urls do proj

--- carrinho/urls.py
+++ carrinho/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.carrinhho_detalhe, name='carrinho'),
+    path('', views.carrinho_detalhe, name='carrinho'),
     path('sucesso/',views.sucesso,name='sucesso'),
 ]
