f935775f88a148964ae1c0905e42fed8fff6784d
Jaqueline Peres <104083695+jaquealt@users.noreply.github.com>
2022-10-13 20:43:09 -0300

    atualizar a base

--- fornecedores/models.py
+++ fornecedores/models.py
@@ -1,3 +1,5 @@
+from asyncio.windows_events import NULL
+from email.policy import default
 from django.contrib.auth.models import User
 from django.db import models
 from endereco.models import Cidades
@@ -6,7 +8,7 @@ from endereco.models import Cidades
 
 class Fornecedores(models.Model):
     fornecedor_nome = models.CharField(max_length=255,verbose_name='Nome')
-    fornecedor_cnpj = models.CharField(max_length=20, verbose_name='CNPJ')
+    fornecedor_cnpj = models.CharField(max_length=20,default=0, verbose_name='CNPJ')
     fornecedor_email = models.EmailField(max_length=100, verbose_name='Email')
     fornecedor_telefone = models.CharField(max_length=30, verbose_name='Telefone')
     fornecedor_logradouro = models.CharField(max_length=100, verbose_name='Logradouro')
@@ -14,8 +16,8 @@ class Fornecedores(models.Model):
     fornecedor_numero = models.IntegerField(default=0, verbose_name='Número')
     fornecedor_cep = models.IntegerField(default=0, verbose_name='CEP')
     fornecedor_complemento = models.CharField(max_length=100, verbose_name='Complemento')
-    fornecedor_obs = models.CharField(max_length=100, verbose_name='Obs')
-    fornecedor_cidade = models.ForeignKey(Cidades,verbose_name='Cidade', on_delete=models.PROTECT)
+    fornecedor_obs = models.CharField(max_length=100,default=NULL, verbose_name='Obs')
+    fornecedor_cidade = models.ForeignKey(Cidades,verbose_name='Cidade',default=0, on_delete=models.PROTECT)
     fornecedor_ativo = models.CharField(max_length=1)
     fornecedor_criem = models.DateTimeField(auto_now_add=True)
     fornecedor_cripor = models.OneToOneField(User,related_name='fornecedor',on_delete=models.CASCADE)
